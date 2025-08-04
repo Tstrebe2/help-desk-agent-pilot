9392e12faf9170764b8ad2380ab85e523aea2a95
Tstrebe2 <tstrebe2@gmail.com>
2025-08-04 17:27:25 -0400

    closes #12: agent now cites tickets used to generate answer

--- help-desk-agent-flask-app/retriever.py
+++ help-desk-agent-flask-app/retriever.py
@@ -3,11 +3,19 @@ from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from pathlib import Path
 
-CHROMA_DB_PATH = Path("../../datasets/help-desk-tickets/chroma_db").resolve()
+_chroma_db_path = Path("../../datasets/help-desk-tickets/chroma_db").resolve()
 
-# Loads the vector store from the specified directory if it already exists
-VECTOR_STORE = Chroma(
-    collection_name="helpdesk_tickets",
-    embedding_function=GoogleGenerativeAIEmbeddings(model="models/embedding-001"),
-    persist_directory=CHROMA_DB_PATH
-)
+def load_helpdesk_ticket_vector_store():
+    """
+    Load a vector store for EHR support tickets using Chroma.
+    Raises an error if the directory does not exist.
+    """
+    if not _chroma_db_path.exists():
+        raise FileNotFoundError(f"Chroma DB directory not found: {_chroma_db_path}")
+
+    vector_store = Chroma(
+        collection_name="helpdesk_tickets",
+        embedding_function=GoogleGenerativeAIEmbeddings(model="models/embedding-001"),
+        persist_directory=_chroma_db_path
+    )
+    return vector_store
---
+++ help-desk-agent-flask-app/ticket_citation_tool.py
@@ -0,0 +1,17 @@
+# ticket_citation_tool.py
+from langchain_core.tools import tool
+from pydantic import BaseModel, Field
+
+class TicketCitationToolArgsSchema(BaseModel):
+    ticket_id: str = Field(
+        description=(
+            "The TicketID for the ticket you would like to cite."
+        )
+    )
+
+@tool(args_schema=TicketCitationToolArgsSchema)
+def ticket_citation_tool(ticket_id: str) -> str:
+    """
+    Tool to format a url reference to a ticket.
+    """
+    return f'<a href="https://index/ticket_view?ticket-id={ticket_id}">[{ticket_id}]</a>'
